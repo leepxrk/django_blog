c0ec0c0a1a0d0b98a332fe766b60a6001dbd6e96
leepxrk <leepx0rrk@hotmail.com>
2018-03-11 22:13:31 +0800

    update sql

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+#create user 'rock'@'localhost' identified by 'qq19900807lrk?';
+
+
+#grant all privileges on django_blog.* to rocklee@localhost identified by '1234';
+
+#grant all privileges on `django_blog`.* to 'rocklee'@'%' identified by 'qq19900807lrk?'; 
+
+
+#grant all privileges on `django_blog`.* to 'rock'@'localhost' identified by 'qq19900807lrk?';  
+
+#flush privileges;
+
+
+#grant all privileges on `testdb`.* to 'test'@'%' identified by '123456';  
--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -73,12 +73,29 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+
+# 默认连接
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+
+# 连接mysql
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog',
+        'USER': 'rock',
+        'PASSWORD': 'qq19900807lrk?',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
@@ -103,9 +120,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-CN'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/BeiJing'
 
 USE_I18N = True
 
