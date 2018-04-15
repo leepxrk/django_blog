aad9733080dede60a936cbb4fcdd1194168adb0f
leepxrk <leepx01rk@hotmail.com>
2018-04-15 12:08:49 +0800

    dd

--- myblog/mblog/admin.py
+++ myblog/mblog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import post
 
 # Register your models here.
+
+#admin.site.register(post)
+
+class postAdmin:
+    list_display = ('title','tag','pub_date')
+
+admin.site.register(post,postAdmin)
--- myblog/mblog/models.py
+++ myblog/mblog/models.py
@@ -1,15 +1,27 @@
 from django.db import models
+from django.utils import timezone
+import imp,sys,re
 
 # Create your models here.
 
+#imp.reload(sys)
+#sys.setdefaultencoding('utf8')
+# python 3 默认编码unicode，所以这一行就不需要了
+
+class musician(models.Model):
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    insturment = models.CharField(max_length = 100)
+
+class Album(models.Model):
+    artist = models.ForeignKey(musician,on_delete=models.CASCADE)
+    name = models.CharField(max_length = 100)
+    release_date = models.DateField()
+    num_stars = models.IntegerField()
+
+
 class post(models.Model):
-    title = models.CharField(max_legth = 200)
-    slug = models.CharField(max_lenth = 200)
-    body = models.Textfield()
-    pub_date = models.DateTimefield(default=timezone.now)
-
-    class Meta:
-        ordering = ('-pub_date')
-
-    def__unicode__(self)
-        return self.title
+    title = models.CharField(max_length = 50)
+    tag = models.CharField(max_length = 50)
+    body = models.TextField()
+    pub_date = models.DateTimeField(default = timezone.now)
